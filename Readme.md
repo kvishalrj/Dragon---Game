9e508e9b22ea21b06355fa43fc2c7e0adc05811d
Vishal Kumar <kvishalrj2020@gmail.com>
2022-09-19 10:52:17 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,23 @@
+![Badge](https://img.shields.io/badge/Project--10-Landing--Page-blue)
+# Dragon - Game💻
+### Hey, I am [**Vishal**](https://www.linkedin.com/in/vishal-kumar-62146b230/) 🙂 
+### This is  my 10th live Project and second Game project.
+### I made this project using ![HTML & CSS & js](https://img.shields.io/badge/HTML%20%26-CSS%20%26%20js-blue)
+
+##Basically I used animation in this project and logics of game.
+
+![](./screenshot/undraw_programmer_re_owql.svg)
+
+ >## Learnings
+ #### I got the understanding of HTML , CSS Animation properties and JavaScript.
+ I learned how a game is developed. 
+
+   
+
+[Click here to view live 🚀](https://mybonton.netlify.app/ "Street Style Landing Page")
+
+## Screenshot of the Project 👇
+![](/images/Screenshot%202022-09-19%20at%2010-46-54%20Dragon%20-%20Dinosour%20Game.png)
+
+
+
---
+++ script.js
@@ -0,0 +1,61 @@
+let dino = document.querySelector('.dino');
+let drag = document.querySelector('.drag');
+let score = document.getElementById('score');
+let gameOver = document.getElementById('gameOver');
+let scoreN = 0;
+let pass = true;
+
+// Dinosour movement code
+document.addEventListener('keydown', (e)=> {    
+    // console.log(e.key);
+    if(e.key=="ArrowUp"){
+        dino.classList.add('dinoAnimateUp');
+        setTimeout(()=>{
+            dino.classList.remove('dinoAnimateUp');
+        },1000);
+    }
+    else if(e.key=="ArrowRight"){
+        let dx = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
+        dino.style.left = `${dx + 250}px`;
+    }
+    else if(e.key=="ArrowLeft"){
+        let dx = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
+        dino.style.left = `${dx - 250}px`;
+    }
+});
+
+// function to check game over
+setInterval((e)=>{
+    let dx = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
+    let dy = parseInt(window.getComputedStyle(dino,null).getPropertyValue('bottom'));
+
+    let ox = parseInt(window.getComputedStyle(drag,null).getPropertyValue('left'));
+    let oy = parseInt(window.getComputedStyle(drag,null).getPropertyValue('bottom'));
+
+    let diffX = Math.abs(dx-ox);
+    let diffY = Math.abs(dy-oy);
+
+    if(diffX<100 && diffY<100){
+        gameOver.style.visibility = "visible";
+        drag.classList.remove('dragAnimateMove');
+        dino.classList.add('dinoAnimateDown');
+        dino.style.bottom = `${-600}px`
+        score.innerText = `Your Score: ${scoreN-1}`;
+    }
+
+    else if(diffX<150 && pass){
+        scoreN += 1;
+        score.innerText = `Your Score: ${scoreN}`;
+        pass = false;
+        setTimeout(()=>{
+            pass = true;
+        }, 1000);
+        setTimeout(()=>{
+            aniDur = parseFloat(window.getComputedStyle(drag, null).getPropertyValue('animation-duration'));
+            newDur = aniDur - 0.1;
+            drag.style.animationDuration = `${newDur}s`;
+        },1200);
+    } 
+
+}, 10);
+
